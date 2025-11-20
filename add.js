bb3f8e1ed6c3f832228c12285aec1c223bde4aaa
RanaCodin <ranaalnofei6@gmail.com>
2025-11-20 21:28:11 +0300

    Add files via upload

---
+++ add.js
@@ -0,0 +1,16 @@
+function showForm(type) {
+
+    const forms = ["adopt", "lost", "sick"];
+
+    forms.forEach(t => {
+        document.getElementById("form-" + t).classList.remove("active");
+        document.getElementById("card-" + t).classList.remove("active");
+    });
+
+    document.getElementById("form-" + type).classList.add("active");
+    document.getElementById("card-" + type).classList.add("active");
+}
+
+window.onload = function () {
+    showForm("adopt");
+};
