5339cf530efa1af2957e941885a33cfcbe755566
janaabdullahofficial-hub <janaabdullahofficial@gmail.com>
2025-12-16 05:06:10 +0300

    Refactor login.php for improved readability and structure

--- login.php
+++ login.php
@@ -51,7 +51,6 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
       <li><a href="adoptable-cats.php">Adopt Cats</a></li>
       <li><a href="lost-cats.php">Lost Cats</a></li>
       <li><a href="sick.php">Sick Cats</a></li>
-      <li><a href="login.php" class="home-btn">Log In</a></li>
     </ul>
   </nav>
 </header>
@@ -133,3 +132,4 @@ if(passwordField && catImg){
 
 </body>
 </html>
+
